e8c7d6221e9ab17816a4917b9eff7c56d55de359
diegosakk <diego.sandoval.sakk@gmail.com>
2023-05-08 21:42:09 -0400

    Agregar archivos de proyecto.

---
+++ BaseSteam/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BaseSteam.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
