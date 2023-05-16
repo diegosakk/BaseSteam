3aa4dc221661bb78284310b54fc043f6283a36ce
diegosakk <diego.sandoval.sakk@gmail.com>
2023-05-15 21:34:16 -0400

    cambios

--- BaseSteam/Controllers/CategoriaController.cs
+++ BaseSteam/Controllers/CategoriaController.cs
@@ -1,12 +1,56 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BaseSteam.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BaseSteam.Controllers
 {
     public class CategoriaController : Controller
     {
+        private BaseSteamContext db = new();
         public IActionResult Index()
         {
+            return View(db.Categoria.ToList());
+
+        }
+        public IActionResult Create()
+        {
             return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Categorium categoria)
+        {
+            db.Add(categoria);
+            db.SaveChanges();
+            //return View();
+            return RedirectToAction("index");
+        }
+        public IActionResult Edit(int? id)
+        {
+            var categoria = db.Categoria.Find(id);
+            if (id != null)
+            {
+                return View(categoria);
+            }
+            return RedirectToAction("index");
+        }
+        [HttpPost]
+        public IActionResult Edit(Categorium categoria)
+        {
+            db.Update(categoria);
+            db.SaveChanges();
+            return RedirectToAction("index");
+        }
+        public IActionResult Delete(int? id)
+        {
+            if (id != null)
+            {
+                var categoria = db.Categoria.Find(id);
+                if (categoria != null)
+                {
+                    db.Categoria.Remove(categoria);
+                    db.SaveChanges();
+                }
+            }
+            return RedirectToAction("index");
         }
     }
 }
--- BaseSteam/Controllers/JuegosController.cs
+++ BaseSteam/Controllers/JuegosController.cs
@@ -1,12 +1,59 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BaseSteam.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace BaseSteam.Controllers
 {
     public class JuegosController : Controller
     {
+        private BaseSteamContext db = new();
         public IActionResult Index()
         {
+            return View(db.Categoria.ToList());
+
+        }
+        public IActionResult Create()
+        {
+            ViewData["IdCategoria"] = new SelectList(db.Categoria, "Id", "Nombre");
+            ViewData["IdDesarrollador"] = new SelectList(db.Desarrolladors, "Id", "Nombre");
             return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Juego juego)
+        {
+            db.Add(juego);
+            db.SaveChanges();
+            //return View();
+            return RedirectToAction("index");
+        }
+        public IActionResult Edit(int? id)
+        {
+            var juego = db.Juegos.Find(id);
+            if (id != null)
+            {
+                return View(juego);
+            }
+            return RedirectToAction("index");
+        }
+        [HttpPost]
+        public IActionResult Edit(Juego juego)
+        {
+            db.Update(juego);
+            db.SaveChanges();
+            return RedirectToAction("index");
+        }
+        public IActionResult Delete(int? id)
+        {
+            if (id != null)
+            {
+                var juego = db.Juegos.Find(id);
+                if (juego != null)
+                {
+                    db.Juegos.Remove(juego);
+                    db.SaveChanges();
+                }
+            }
+            return RedirectToAction("index");
         }
     }
 }
